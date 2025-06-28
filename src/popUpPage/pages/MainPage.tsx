f7686a7cb46254d653a11b15cfb76f79d98429e1
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-28 17:12:50 +0900

    feat: 설정 페이지 이동 버튼 구현 #1

--- src/popUpPage/pages/MainPage.tsx
+++ src/popUpPage/pages/MainPage.tsx
@@ -1,9 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MainPage() {
+  const navigate = useNavigate();
+
+  const navigateToSettingPage = () => {
+    navigate("/setting");
+  };
+
   return (
     <>
       <h2 className="text-3xl mb-4 font-black">DonuTool</h2>
-      <button id="fullscreenButton" className="px-3.5 py-2 flex cursor-pointer items-center justify-center rounded-xl text-gray-600 bg-gray-100 p-1 shadow transition duration-300 hover:shadow-md">
+      <button
+        id="fullscreenButton"
+        className="font-semibold px-3.5 py-2 flex cursor-pointer items-center justify-center rounded-xl text-gray-600 bg-gray-100 p-1 shadow transition duration-300 hover:shadow-md">
         전체화면
+      </button>
+      <button
+        onClick={() => navigateToSettingPage()}
+        className="font-semibold absolute top-3 text-xs right-3 px-3.5 py-2 flex cursor-pointer items-center justify-center rounded-full text-gray-600 bg-gray-100 p-1 shadow transition duration-300 hover:shadow-md">
+        설정
       </button>
     </>
   );

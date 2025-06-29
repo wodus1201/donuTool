7965eafc5b2f2be2c97b577e4a681fa80b27a84a
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-29 16:07:35 +0900

    chore: manifest 파일 접근방식 변경 #2

--- src/popUpPage/pages/SettingPage.tsx
+++ src/popUpPage/pages/SettingPage.tsx
@@ -1,10 +1,10 @@
 export default function SettingPage() {
   return (
     <>
-      <div className="text-2xl font-bold mb-4">설정</div>
-      <div className="rounded-2xl w-[200px] h-[150px] mb-4 flex justify-center items-center bg-white">툴바UI</div>
+      <div className="text-2xl font-bold mb-7">설정</div>
+      <div className="rounded-2xl w-[200px] h-[150px] mb-7 flex justify-center items-center bg-white">툴바UI</div>
       <div className="w-[300px] h-[100px] flex justify-center items-center">
-        <div className="grid grid-cols-5 grid-rows-2 gap-1 w-full h-full">
+        <div className="grid grid-cols-5 grid-rows-2 gap-2 w-full h-full">
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className="flex items-center justify-center bg-white rounded-full text-xs">
               상호작용{i + 1}
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,39 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/background/*",
+          dest: "background",
+        },
+        {
+          src: "src/content/*",
+          dest: "content",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve("src"),
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });

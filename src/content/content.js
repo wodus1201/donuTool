f1ff96ea4d0a2027fcb885a5e3f0e58e06d82fad
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-30 17:13:48 +0900

    feat: 툴바UI CSS 대신 정적 이미지 파일로 변경 #2

--- src/content/content.js
+++ src/content/content.js
@@ -9,18 +9,16 @@ chrome.runtime.onMessage.addListener((message) => {
   }
 });
 
-const toolBarUI = document.createElement("div");
+const toolBarUI = document.createElement("img");
 toolBarUI.id = "donuTool-toolBar";
+toolBarUI.src = chrome.runtime.getURL("assets/donuToolBar.png");
 
 Object.assign(toolBarUI.style, {
   position: "absolute",
   width: "180px",
   height: "180px",
-  borderRadius: "50%",
   pointerEvents: "none",
   zIndex: 9999,
-  maskImage: "radial-gradient(circle at center, transparent 35px, black 11px)",
-  background: "conic-gradient(from -110deg, transparent 0deg 130deg, rgb(196, 196, 196) 130deg 360deg)",
   transition: "opacity 0.3s ease, transform 0.3s ease",
 });
 document.body.appendChild(toolBarUI);
@@ -51,8 +49,8 @@ document.addEventListener(
 );
 
 function updateToolBarUIPosition() {
-  toolBarUI.style.left = lastCursorPosition.x - 85 + "px";
-  toolBarUI.style.top = lastCursorPosition.y - 75 + "px";
+  toolBarUI.style.left = lastCursorPosition.x - 83 + "px";
+  toolBarUI.style.top = lastCursorPosition.y - 73 + "px";
 }
 
 function checkCursorEvent(element) {

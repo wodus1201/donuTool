d197a81edae7b359ee380d2f1c71a7a8bcbe9e1c
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-30 19:21:29 +0900

    chore: 옵션 페이지 툴바UI 이미지 추가 #2

--- src/popUpPage/pages/SettingPage.tsx
+++ src/popUpPage/pages/SettingPage.tsx
@@ -1,16 +1,17 @@
 import GoBackButton from "@/popUpPage/components/GoBackButton";
+import donuToolBar from "@/assets/donuToolBar.png";
 
 export default function SettingPage() {
   return (
     <>
       <GoBackButton />
       <div className="text-2xl font-bold mb-7">설정</div>
-      <div className="rounded-2xl w-[200px] h-[150px] mb-7 flex justify-center items-center bg-white">툴바UI</div>
-      <div className="w-[300px] h-[100px] flex justify-center items-center">
-        <div className="grid grid-cols-5 grid-rows-2 gap-2 w-full h-full">
+      <img src={donuToolBar} className="w-[150px] h-[150px] mb-7"></img>
+      <div className="flex justify-center items-center mb-5">
+        <div className="grid grid-cols-5 grid-rows-2 gap-2">
           {Array.from({ length: 10 }).map((_, i) => (
-            <div key={i} className="flex items-center justify-center bg-white rounded-full text-xs">
-              상호작용{i + 1}
+            <div key={i} className="flex items-center justify-center bg-white w-[35px] h-[35px] rounded-full text-xs font-black text-gray-500">
+              {i + 1}
             </div>
           ))}
         </div>
